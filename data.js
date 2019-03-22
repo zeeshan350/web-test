6cebe4656200d0b933f3dcadd197f0c339a2b137
Zeeshan Munawar <zeeshanmunawar@live.com>
2019-03-22 16:48:48 +0000

    Add files via upload

---
+++ data.js
@@ -0,0 +1,223 @@
+var data = [
+  {
+    "id": "01",
+    "firstName": "Simon",
+    "lastName": "Date",
+    "jobTitle": "Web Developer",
+    "reportsTo": "Instructional Technology Manager",
+    "dateJoined": "2017-01-01T09:00:00.000Z"
+  }, {
+    "id": "02",
+    "firstName": "Anna",
+    "lastName": "Wood",
+    "jobTitle": "Director of King's Online",
+    "reportsTo": "",
+    "dateJoined": "2019-01-01T09:00:00.000Z"
+  }, {
+    "id": "03",
+    "firstName": "Eden",
+    "lastName": "Lazaness",
+    "jobTitle": "Head of Design and Innovation",
+    "reportsTo": "Director of King's Online",
+    "dateJoined": "2015-01-01T09:00:00.000Z"
+  }, {
+    "id": "04",
+    "firstName": "Rachel",
+    "lastName": "Brown",
+    "jobTitle": "Web Designer",
+    "reportsTo": "Head of Design and Innovation",
+    "dateJoined": "2019-01-01T09:00:00.000Z"
+  }, {
+    "id": "05",
+    "firstName": "Janek",
+    "lastName": "Zavatski",
+    "jobTitle": "Web Designer",
+    "reportsTo": "Head of Design and Innovation",
+    "dateJoined": "2014-01-01T09:00:00.000Z"
+  }, {
+    "id": "06",
+    "firstName": "Shirley",
+    "lastName": "Xie",
+    "jobTitle": "Web Designer",
+    "reportsTo": "Head of Design and Innovation",
+    "dateJoined": "2019-01-01T09:00:00.000Z"
+  }, {
+    "id": "07",
+    "firstName": "Rebecca",
+    "lastName": "Byrne",
+    "jobTitle": "E-learning Visual Designer",
+    "reportsTo": "Instructional Technology Manager",
+    "dateJoined": "2019-01-01T09:00:00.000Z"
+  }, {
+    "id": "08",
+    "firstName": "Jonathan",
+    "lastName": "Pott-Negrine",
+    "jobTitle": "E-learning Visual Designer",
+    "reportsTo": "Instructional Technology Manager"
+  },
+  {
+    "id": "09",
+    "firstName": "Louise",
+    "lastName": "Bennett",
+    "jobTitle": "Instructional Technology Manager",
+    "reportsTo": "Head of Design and Innovation",
+    "dateJoined": "2016-01-01T09:00:00.000Z"
+  },
+  {
+    "id": "10",
+    "firstName": "Paul",
+    "lastName": "Gillary",
+    "jobTitle": "Learning Technologist",
+    "reportsTo": "Instructional Technology Manager",
+    "dateJoined": "2019-01-01T09:00:00.000Z"
+  }, {
+    "id": "11",
+    "firstName": "Lola",
+    "lastName": "Nezianya",
+    "jobTitle": "E-learning Visual Designer",
+    "reportsTo": "Instructional Technology Manager",
+    "dateJoined": "2019-01-01T09:00:00.000Z"
+  }, {
+    "id": "12",
+    "firstName": "Sam",
+    "lastName": "Hodgson",
+    "jobTitle": "E-learning Visual Designer",
+    "reportsTo": "Instructional Technology Manager",
+    "dateJoined": "2019-01-01T09:00:00.000Z"
+  }, {
+    "id": "13",
+    "firstName": "Danielle",
+    "lastName": "Johnstone",
+    "jobTitle": "E-learning Visual Designer",
+    "reportsTo": "Instructional Technology Manager",
+    "dateJoined": "2019-01-01T09:00:00.000Z"
+  }, {
+    "id": "14",
+    "firstName": "Abi",
+    "lastName": "Lingford",
+    "jobTitle": "E-learning Visual Designer",
+    "reportsTo": "Instructional Technology Manager",
+    "dateJoined": "2019-01-01T09:00:00.000Z"
+  }, {
+    "id": "15",
+    "firstName": "Karen",
+    "lastName": "Greetham",
+    "jobTitle": "Head of Production",
+    "reportsTo": "Director of King's Online",
+    "dateJoined": "2019-01-01T09:00:00.000Z"
+  }, {
+    "id": "16",
+    "firstName": "Duncan",
+    "lastName": "Smith",
+    "jobTitle": "Head of Strategy, Planning & Business Management",
+    "reportsTo": "Director of King's Online",
+    "dateJoined": "2019-01-01T09:00:00.000Z"
+  }, {
+    "id": "17",
+    "firstName": "Ashleigh",
+    "lastName": "Collins",
+    "jobTitle": "Communications & Student Engagement Manager",
+    "reportsTo": "Head of Strategy, Planning & Business Management",
+    "dateJoined": "2019-01-01T09:00:00.000Z"
+  }, {
+    "id": "18",
+    "firstName": "Lauren",
+    "lastName": "Perry",
+    "jobTitle": "Operations Officer",
+    "reportsTo": "Head of Strategy, Planning & Business Management",
+    "dateJoined": "2019-01-01T09:00:00.000Z"
+  }, {
+    "id": "19",
+    "firstName": "Anna",
+    "lastName": "Sharples",
+    "jobTitle": "Project Manager",
+    "reportsTo": "Head of Production",
+    "dateJoined": "2019-01-01T09:00:00.000Z"
+  }, {
+    "id": "20",
+    "firstName": "Avninder",
+    "lastName": "Grewal",
+    "jobTitle": "Project Manager",
+    "reportsTo": "Head of Production",
+    "dateJoined": "2019-01-01T09:00:00.000Z"
+  }, {
+    "id": "21",
+    "firstName": "Jessica",
+    "lastName": "Wyatt",
+    "jobTitle": "Project Manager",
+    "reportsTo": "Head of Production",
+    "dateJoined": "2019-01-01T09:00:00.000Z"
+  }, {
+    "id": "22",
+    "firstName": "Jasmyn",
+    "lastName": "Feakes",
+    "jobTitle": "Project Manager",
+    "reportsTo": "Head of Production",
+    "dateJoined": "2019-01-01T09:00:00.000Z"
+  }, {
+    "id": "23",
+    "firstName": "Rachele",
+    "lastName": "Wall",
+    "jobTitle": "E-learning Assistant",
+    "reportsTo": "Head of Production",
+    "dateJoined": "2018-01-01T09:00:00.000Z"
+  }, {
+    "id": "24",
+    "firstName": "Alessandra",
+    "lastName": "Fiorio",
+    "jobTitle": "E-learning Assistant",
+    "reportsTo": "Head of Production",
+    "dateJoined": "2019-01-01T09:00:00.000Z"
+  }, {
+    "id": "25",
+    "firstName": "Paul",
+    "lastName": "Warren",
+    "jobTitle": "E-learning Assistant",
+    "reportsTo": "Head of Production",
+    "dateJoined": "2019-01-01T09:00:00.000Z"
+  },
+  {
+    "id": "26",
+    "firstName": "Daniel",
+    "lastName": "Childs",
+    "jobTitle": "Senior Video Producer",
+    "reportsTo": "Head of Design and Innovation",
+    "dateJoined": "2019-01-01T09:00:00.000Z"
+  },
+  {
+    "id": "27",
+    "firstName": "Liva",
+    "lastName": "Puce",
+    "jobTitle": "Video Producer",
+    "reportsTo": "Senior Video Producer",
+    "dateJoined": "2019-01-01T09:00:00.000Z"
+  }, {
+    "id": "28",
+    "firstName": "Georg",
+    "lastName": "Avram",
+    "jobTitle": "Video Producer",
+    "reportsTo": "Senior Video Producer",
+    "dateJoined": "2019-01-01T09:00:00.000Z"
+  }, {
+    "id": "28",
+    "firstName": "Jose",
+    "lastName": "Pava Cano",
+    "jobTitle": "Video Producer",
+    "reportsTo": "Senior Video Producer",
+    "dateJoined": "2019-01-01T09:00:00.000Z"
+  }, {
+    "id": "29",
+    "firstName": "Fernando",
+    "lastName": "Barros Machado",
+    "jobTitle": "Video Editor",
+    "reportsTo": "Senior Video Producer",
+    "dateJoined": "2019-01-01T09:00:00.000Z"
+  }, {
+    "id": "30",
+    "firstName": "George",
+    "lastName": "Knowles",
+    "jobTitle": "Video Editor",
+    "reportsTo": "Senior Video Producer",
+    "dateJoined": "2019-01-01T09:00:00.000Z"
+  }
+];
